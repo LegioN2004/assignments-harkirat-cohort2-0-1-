4cb2b03fd1b9547ba9aa790e6b284e25502f4a8c
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-12-19 15:47:13 +0530

    solved solutions of week 1 hw

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,27 @@
 */
 
 function isAnagram(str1, str2) {
+  // if (str1.length == str2.length) {
+  //   return true;
+  // }
+  // str1 = str1.toLowerCase();
+  // str1 = str2.toLowerCase();
 
+  // let arr1 = str1.split("").sort();
+  // let arr2 = str2.split("").sort();
+
+  // function check(arr1, arr2) {
+  //   for (let i = 0; i < arr1.length; i++) {
+  //     if (arr1[i] == arr2[i]) {
+  //       return true;
+  //     }
+  //   }
+  // }
+  // let ans = check(arr1, arr2);
+  // return ans;
+
+  str1 = str1.toUpperCase().split("").sort().join("");
+  str2 = str2.toUpperCase().split("").sort().join("");
+  return str1 == str2;
 }
-
 module.exports = isAnagram;
